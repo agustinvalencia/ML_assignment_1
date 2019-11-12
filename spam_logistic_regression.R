5275dd4246c320d08bb3a42fceb3a1f4d70bb6a1
Agustín Valencia <valencia.ag@gmail.com>
2019-11-12 09:51:02 +0100

    Q1 first candidate

--- spam_logistic_regression.R
+++ spam_logistic_regression.R
@@ -1,5 +1,6 @@
 ## 0. Importing data
-data <- read.csv2("data/spambase.csv")
+library(openxlsx)
+data <- read.xlsx("data/spambase.xlsx")
 
 ## 1. Splitting data (from instructions)
 n = dim(data)[1]
@@ -8,14 +9,35 @@ id=sample(1:n, floor(n*0.5))
 train=data[id,]
 test=data[-id,]
 
-## 2. Logistic regression to classify 
+
+
+## 2. Logistic regression to classify threshold = 0.5
 fit <- glm(Spam ~ . , data = train, family = "binomial")
-summary(fit)
-predictions <- predict(fit, newdata = test, type="response")
+predictions <- predict(fit, newdata = test)
 
 # classifying it
-results <- as.integer(predictions > 0.5)
+results_05 <- as.integer(predictions > 0.5)
+table( test$Spam, results_05)
+
+
+
+## 3. Logistic regression to classify threshold = 0.8
+results_08 <- as.integer(predictions > 0.8)
+table(test$Spam, results_08)
+
+
+
+## 4. Train KNN K=30
+knn_model <- train.kknn(Spam ~ . , data = train, k = 30)
+knn_fit <- predict(knn_model, test)
+results <- as.integer(knn_fit > 0.5)
 table(test$Spam, results)
 
 
+
+## 5. Train KNN K=1
+knn_model <- train.kknn(Spam ~ . , data = train, k = 1)
+knn_fit <- predict(knn_model, test)
+results <- as.integer(knn_fit > 0.5)
+table(test$Spam, results)
 
