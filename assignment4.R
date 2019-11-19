fa3648b18291c7e1d63ad911c7d3f6f390495973
Agustín Valencia <valencia.ag@gmail.com>
2019-11-19 10:14:00 +0100

    assignment4.R

--- assignment4.R
+++ assignment4.R
@@ -20,9 +20,59 @@ P <- data.frame(
     P5 = p^5, 
     P6 = p^6 
 )
-M1 <- lm(Y ~ ., data = P[,1:2])
-M2 <- lm(Y ~ ., data = P[,1:3])
-M3 <- lm(Y ~ ., data = P[,1:4])
-M4 <- lm(Y ~ ., data = P[,1:5])
-M5 <- lm(Y ~ ., data = P[,1:6])
-M6 <- lm(Y ~ ., data = P[,1:7])
+
+n = dim(P)[1]
+set.seed(12345)
+id = sample(1:n, floor(n*0.5))
+train = P[id,]
+test = P[-id,]
+
+M1 <- lm(Y ~ ., data = train[,1:2])
+M2 <- lm(Y ~ ., data = train[,1:3])
+M3 <- lm(Y ~ ., data = train[,1:4])
+M4 <- lm(Y ~ ., data = train[,1:5])
+M5 <- lm(Y ~ ., data = train[,1:6])
+M6 <- lm(Y ~ ., data = train[,1:7])
+models <- list(
+  M1 = M1, 
+  M2 = M2, 
+  M3 = M3, 
+  M4 = M4, 
+  M5 = M5, 
+  M6 = M6
+)
+
+## Train Scores
+eval_model <- function(model, data) {
+  pred <- predict(model, data)
+  errors <- pred - data$Y
+  MSE <- mean(errors^2)
+  return(MSE)
+}
+
+MSE_train <- c()
+MSE_train[1] <- eval_model(M1, train)
+MSE_train[2] <- eval_model(M2, train)
+MSE_train[3] <- eval_model(M3, train)
+MSE_train[4] <- eval_model(M4, train)
+MSE_train[5] <- eval_model(M5, train)
+MSE_train[6] <- eval_model(M6, train)
+
+MSE_test <- c()
+MSE_test[1] <- eval_model(M1, test)
+MSE_test[2] <- eval_model(M2, test)
+MSE_test[3] <- eval_model(M3, test)
+MSE_test[4] <- eval_model(M4, test)
+MSE_test[5] <- eval_model(M5, test)
+MSE_test[6] <- eval_model(M6, test)
+
+
+
+
+
+
+
+
+
+
+
